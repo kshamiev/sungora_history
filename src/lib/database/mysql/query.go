bf861fddf12839de8a52cd8b47080c247189a06a
Konstantin Shamiev <konstantin@shamiev.ru>
2015-06-16 19:13:39 +0300

    fix

--- src/lib/database/mysql/query.go
+++ src/lib/database/mysql/query.go
@@ -6,13 +6,6 @@ var Query = make(map[string][]string)
 // Инициализация базы запросов к БД
 func init() {
 	Query = map[string][]string{
-		`base/controllers`: []string{
-			"UPDATE `Controllers` SET `Position` = `Position` - 1 WHERE `Position` > ?;",
-			"UPDATE `Controllers` SET `Position` = `Position` + 1 WHERE `Position` > ?;",
-			"UPDATE `Controllers` SET `Position` = ? WHERE `Id` = ?;",
-			"DELETE FROM GroupsUri WHERE Controllers_Id = ? AND Groups_Id = ?;",
-			"SELECT MAX(Position) as Max FROM `Controllers`;",
-		},
 		`base/uri`: []string{
 			"UPDATE `Uri` SET `Position` = `Position` - 1 WHERE `Position` > ?;",
 			"UPDATE `Uri` SET `Position` = `Position` + 1 WHERE `Position` > ?;",
@@ -25,6 +18,13 @@ func init() {
 			"INSERT UsersGroups SET `Users_Id` = ?, `Groups_Id` = ?;",
 			"DELETE FROM UsersGroups WHERE Users_Id = ? AND Groups_Id = ?;",
 			"UPDATE Users SET `DateOnline` = ?, `Token` = ? WHERE Id = ?;",
+		},
+		`base/controllers`: []string{
+			"UPDATE `Controllers` SET `Position` = `Position` - 1 WHERE `Position` > ?;",
+			"UPDATE `Controllers` SET `Position` = `Position` + 1 WHERE `Position` > ?;",
+			"UPDATE `Controllers` SET `Position` = ? WHERE `Id` = ?;",
+			"DELETE FROM GroupsUri WHERE Controllers_Id = ? AND Groups_Id = ?;",
+			"SELECT MAX(Position) as Max FROM `Controllers`;",
 		},
 	}
 }

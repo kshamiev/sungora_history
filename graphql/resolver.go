7cd9c743b69020b5065c160c4ca30b53621aed54
kshamiev <konstantin@shamiev.ru>
2019-10-13 22:29:36 +0300

    feature gq

--- graphql/resolver.go
+++ graphql/resolver.go
@@ -22,5 +22,22 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (*Todo
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*Todo, error) {
-	panic("not implemented")
+	data := []*Todo{{
+		ID:   "uid 1",
+		Text: "popcorn 1",
+		Done: false,
+		User: nil,
+	}, {
+		ID:   "uid 2",
+		Text: "popcorn 2",
+		Done: false,
+		User: nil,
+	}, {
+		ID:   "uid 3",
+		Text: "popcorn 3",
+		Done: false,
+		User: nil,
+	},
+	}
+	return data, nil
 }
